7247ee1fda8ac50f3d25cfa35474c39050eaae6a
yiranzi <570932296@qq.com>
2019-07-09 17:34:37 +0800

    init

---
+++ precache-manifest.b7d306333b409226cf3e1a5444d56217.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d4ce12ff805017324bbe5a491f32db2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aff81b3b0d2e0a0262eb",
+    "url": "/static/css/main.40bbc1eb.chunk.css"
+  },
+  {
+    "revision": "2c8c57f0e324812da264",
+    "url": "/static/js/2.64604142.chunk.js"
+  },
+  {
+    "revision": "aff81b3b0d2e0a0262eb",
+    "url": "/static/js/main.7bf2159b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
