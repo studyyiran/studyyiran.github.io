0dc46d831f3d80a478053ce75c1ca8a4df61046b
sunyiran <sunyiran@zhihu.com>
2022-05-14 15:46:41 +0800

    feat: 设置最大长度

--- index.js
+++ index.js
@@ -1,42 +1,40 @@
 console.log('hello world')
-// alert('hello world')
 
 const $ = (...arg) => {
-    return document.querySelector(...arg)
+  return document.querySelector(...arg)
 }
 
-let name = "02"
+let name = '02'
 
-const myModal = () => {
-    
-}
-
+const myModal = () => {}
 
 const init = () => {
-    let myButton = $('button');
-    console.log('init')
-    console.log(myButton)
-    if (myButton) {
-        myButton.onclick = getYourName
-    }
+  let myButton = $('button')
+  console.log('init')
+  console.log(myButton)
+  if (myButton) {
+    myButton.onclick = getYourName
+  }
 }
 
 const getYourName = () => {
-    name = prompt('请输入你的名字。');
+  name = prompt('请输入你的名字。')
 }
 
 const run = () => {
-    const test = 
-    console.log($('html'))
-    const target = $('li:last-child')
-    if (target) {
-        console.log(target)
+  console.log($('html'))
+  const target = $('li:last-child')
+  if (target) {
+    console.log(target)
+  }
+  let count = 0
+  setInterval(() => {
+    count++
+    if (count < 3) {
+      target.textContent = target.textContent + (name || '11')
     }
-    setInterval(() => {
-        target.textContent = target.textContent + (name || '11')
-    }, 1000)
+  }, 1000)
 }
-
 
 init()
 run()
